e78cccff20b08727ddebde7a63aad0c61ea7ad17
Karthik_Jangam <102133551+JangamKarthik@users.noreply.github.com>
2024-04-01 10:38:40 +0530

    Nothing important

--- src/features/common/Footer.js
+++ src/features/common/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div className="max-w-2xl mx-auto text-white py-10">
           <div className="text-center">
             <h3 className="text-3xl mb-3"> Download our Ecommerce App </h3>
-            <p> Buy what you want. </p>
+            <p> Abhi kuch socha nahi. </p>
             <div className="flex justify-center my-10">
               <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
                 <img
@@ -32,7 +32,7 @@ function Footer() {
           <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
             <p className="order-2 md:order-1 mt-8 md:mt-0">
               {' '}
-              © CoderDost, 2023.{' '}
+              © Nahi Hai, 2023.{' '}
             </p>
             <div className="order-1 md:order-2">
               <span className="px-2">About us</span>
